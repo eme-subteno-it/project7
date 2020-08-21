1e012984bf9f2fd7539b8d5bad10c441329b45bf
Elodie Meunier <eme@subteno-it.com>
2020-08-21 15:27:31 +0200

    [FIX] Optimisation code

--- app_py/models/Map.py
+++ app_py/models/Map.py
@@ -1,15 +1,27 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import googlemaps
-import requests
 from config import API_KEY_GOOGLE
 
 
+# pylint: disable=invalid-name, too-few-public-methods
 class Map:
+    """
+        This class the coordinate receipt by the user.
+        She use the GoogleMap API.
+        :param arg1: The location name receipt by the user
+    """
 
     def __init__(self, location):
         self.location = location
 
     def get_geocode(self):
+        """
+            This method connects to the GoogleMap API using an API key
+            and uses its geocode method to retrieve and return the coordinates
+            of the location chosen by the user.
+        """
         gmaps = googlemaps.Client(API_KEY_GOOGLE)
         geocode_result = gmaps.geocode(self.location)
         address = {}
@@ -20,5 +32,5 @@ class Map:
             address['geometry'] = geocode_result[0]['geometry']['location']
 
             return address
-        else:
-            return False
+
+        return False
--- app_py/models/__init__.py
+++ app_py/models/__init__.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from . import Parsing_flask
 from . import Map
 from . import Wiki
