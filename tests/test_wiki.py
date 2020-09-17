4da2f5dbd3d9c0c8b568d3ad5e12a251b97b0439
Elodie Meunier <eme@subteno-it.com>
2020-09-17 15:05:48 +0200

    [IMP] - Change the values in test_wiki

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -8,30 +8,30 @@ import requests
 class TestWiki:
 
     def setup_method(self):
-        keyword = "Cité Paradis"
+        keyword = "Quai de la Charente"
         self.wiki = Wiki(keyword)
 
     def test_get_page_id(self):
         data = {
             '5653202': {
-                'pageid': 5653202,
+                'pageid': 3120618,
                 'ns': 0,
-                'title': 'Cité Paradis',
+                'title': 'Quai de la Charente',
                 'index': 1,
-                'extract': 'La cité Paradis est une voie publique située dans le 10e arrondissement de Paris.'
+                'extract': 'Le quai de la Charente est un quai situé le long du canal Saint-Denis, à Paris, dans le 19e arrondissement. \n\n'
             }
         }
         get_page_id = self.wiki.get_page_id(data)
         assert get_page_id == '5653202'
-    
+
     def test_get_story(self, monkeypatch):
-        result =  {
-            'extract': 'La cité Paradis est une voie publique située dans le 10e arrondissement de Paris.',
-            'url': 'https://fr.wikipedia.org/wiki/Cit%C3%A9_Paradis',
+        result = {
+            'extract': 'Le quai de la Charente est un quai situé le long du canal Saint-Denis, à Paris, dans le 19e arrondissement. \n\n',
+            'url': 'https://fr.wikipedia.org/wiki/Quai_de_la_Charente',
         }
 
         def mockreturn(request):
             return BytesIO(json.dumps(result).encode())
-        
+
         monkeypatch.setattr(requests, 'get', mockreturn)
         assert self.wiki.get_story() == result
